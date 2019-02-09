5729d6660e1e7250289b033cebceed9471f0b014
Yeshi <tentibet59@gmail.com>
2019-02-09 18:25:56 +0530

    Update and rename README.md to index.php

---
+++ index.php
@@ -0,0 +1,52 @@
+<html>
+<head>
+<title>Contact Us</title>
+<link rel="stylesheet" type="text/css" href="form.css" />
+<link rel="stylesheet" href="myhome.css" type="text/css" />
+
+
+</head>
+<body>
+<div class="left-side">
+
+<!-- Logo -->
+<div id="logo" style="width: 246px;">
+<a href="../index.php"><img src="../assets/image/adeza.png" alt="Adeza"></a>
+</div>
+
+
+
+<div class="contact-title">
+<h1>Contact Us</h1>
+</div>
+
+<div class="contact-form">
+<form id="contact-form" method="POST" action="contact-handler.php">
+<input name="name" type="text" class="form-control" placeholder="Your Name" required>
+<br>
+<input name="email" type="email" class="form-control" placeholder="Your Email" required><br>
+
+<textarea name="message" class="form-control" placeholder="Message" rows="4" required></textarea><br>
+
+<input type="submit" class="form-control submit" value="SEND MESSAGE">
+</form>
+</div>
+
+</div>
+
+<div align="center">
+<ul>
+          <li><i class="glyphicon glyphicon-map-marker" aria-hidden="true"></i><strong><span style="color:white;">Address</span></strong><span style="color:white;"> : No 8/2, Ulsoor Rd, Yellappa Chetty Layout, Ulsoor, Bengaluru, Karnataka 560042</span></li>
+          <li><i class="glyphicon glyphicon-envelope" aria-hidden="true"></i><strong><span style="color:white;">Email</span></strong>  : <a href="mailto:info@oswintechnologies.com">info@oswintechnologies.com</a></li>
+          <li><i class="glyphicon glyphicon-earphone" aria-hidden="true"></i><strong><span style="color:white;">Phone </span></strong><span style="color:white;"> : +91 9739327547</span></li>
+        </ul>
+</div>
+
+<div align="center">
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15551.796588741816!2d77.619541!3d12.9751044!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x8db72bf7edbf732a!2sNovel+Office!5e0!3m2!1sen!2sin!4v1549623249880" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
+
+
+
+</body>
+</html>
